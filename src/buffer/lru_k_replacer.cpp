6151366ccf98122678ab31fe0905e286cf6f22fb
tanao0308 <tanao030801@foxmail.com>
2024-06-10 14:24:32 +0800

    working on lru_k_replacer

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -15,16 +15,43 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+/*
+replacer_size_ = 初始大小 - 不能被移除的帧数量
+*/
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+	node_store_.clear();
+}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+	return false;
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+	if(node_store_.find(frame_id) == node_store_.end())
+	{
+		node_store_.emplace(current_timestamp_, frame_id, LRUKNode(k_, fid));
+	}
+	else
+	{
+		node_store_.at(frame_id).add_log(current_timestamp_);
+	}
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+
+}
+
+/*
+buffer内可移除的帧的数量，不超过replacer_size_
+*/
+auto LRUKReplacer::Size() -> size_t {
+	return curr_size_;
+	return 0;
+}
 
 }  // namespace bustub
